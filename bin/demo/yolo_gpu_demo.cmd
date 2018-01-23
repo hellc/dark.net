a6825a26efca86a96918cee7387b4db724e44688
ivanmanov <ivanmanov@live.com>
2018-01-23 12:17:59 +0100

    dependencies updated, project restructed

---
+++ bin/demo/yolo_gpu_demo.cmd
@@ -0,0 +1,3 @@
+..\yolo_gpu.exe detector demo C:\darknet\data\voc.names C:\darknet\cfg\yolo-voc.cfg C:\darknet\weights\yolo-voc.weights -thresh 0.24 http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_30mb.mp4
+
+pause
